018b7182f4b96694b4fefb33c7f8d27d9409aa3c
John Tabash <jrtabash@yahoo.com>
2020-11-27 09:29:02 -0600

    Added max contiguous sum puzzle.

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # Misc Coding Puzzles
 Solutions to miscellaneous coding puzzles.
 
-All puzzle solutions in repo use static python typing, and have all been tested with mypy static type checker.
+All puzzle solutions in repo use static python typing, and have all been analyzed with mypy static type checker.
 
 Solved puzzles include:
 1. [Eight Queens Problem](eight_queens.py)
 2. [Generate Primes up to N](generate_primes.py)
 3. [Check directed graph has cycles](graph_cycle.py)
+4. [Max Continguous Sum](max_contiguous_sum.py)
---
+++ max_contiguous_sum.py
@@ -0,0 +1,68 @@
+"""
+Given a list of integers of any length, calculate the maximum
+contiguous sum of all sub ranges.
+
+For instance, given the list [1, -2, 3, 2, -1], the maximum
+contiguous sum is 5, which is the sum of the sub range [3, 2].
+"""
+
+from typing import Sequence
+
+Integers = Sequence[int]
+
+def mcs_linear(nums: Integers) -> int:
+    """
+    Calculate maximum contiguous sum for given sequence of integers.
+    Complexity: O(n)
+    """
+
+    size: int = len(nums)
+    if size == 0:
+        return 0
+
+    max_sum: int = nums[0]
+    cur_sum: int = 0
+    for i in range(size):
+        cur_sum += nums[i]
+        if cur_sum > max_sum:
+            max_sum = cur_sum
+        if cur_sum < 0:
+            cur_sum = 0
+
+    return max_sum
+
+def mcs_quadratic(nums: Integers) -> int:
+    """
+    Calculate maximum contiguous sum for given sequence of integers.
+    Complexity: O(n^2)
+    """
+
+    size: int = len(nums)
+    if size == 0:
+        return 0
+
+    max_sum: int = nums[0]
+    for i in range(size):
+        cur_sum: int = 0
+        for j in range(i, size):
+            cur_sum += nums[j]
+            if cur_sum > max_sum:
+                max_sum = cur_sum
+
+    return max_sum
+
+def test_mcs(nums: Integers, mcs_actual: int) -> None:
+    """
+    Calculate maximum contiguous sum, using both linear and quadratic functions,
+    and display actual and calculated maximums.
+    """
+
+    print("ListOfInts:", nums)
+    print("MCS Actual:", mcs_actual)
+    print("MCS Linear:", mcs_linear(nums))
+    print("MCS Quadra:", mcs_quadratic(nums))
+
+if __name__ == "__main__":
+    test_mcs([1, -2, 3, 2, -1], 5)
+    test_mcs([-3, 1, -2, 2, 3, 1, -5, 3, 2], 6)
+    test_mcs([6, -5, -8, 9, 7, 10, 4, -3, 8, -5], 35)
